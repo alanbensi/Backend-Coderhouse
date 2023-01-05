3e515ac66ddf6a771595c209f79c741233e7bd17
alan bensi <alanbensi9@gmail.com>
2023-01-05 01:57:32 -0300

    First commit. Im working on my first challenge creating the class productManager

---
+++ productManajer.js
@@ -0,0 +1,19 @@
+class ProductManager { 
+    products; 
+
+    constructor() {
+        this.products = [];
+    }
+    
+    addProducts(title, description, price, thumbnail, code, stock) {
+        let newProduct = {
+            id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        } 
+    }
+}
