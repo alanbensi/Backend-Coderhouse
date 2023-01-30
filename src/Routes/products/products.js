34efa0d1026518635b133cc1625aad1993518e75
alan bensi <alanbensi9@gmail.com>
2023-01-30 01:27:36 -0300

    CRUD of products is ready. The validations aren´t worked yet

--- src/Routes/products/products.js
+++ src/Routes/products/products.js
@@ -14,7 +14,7 @@ productsRouter.get("/", async (req, res) => {
     }
 });
 
-productsRouter.get ("/:pid", async (req,res)=> {
+productsRouter.get ("/:pid", async (req,res) => {
     const productId = req.params.pid;
     const product = await manager.getProductByID(parseInt(productId));
     res.send({ status: "Success", product: product });
@@ -27,8 +27,18 @@ productsRouter.post ("/", async (req, res) => {
     res.status(400).send ({status: "Error", message: "The request has an error"}); 
 })
 
-productsRouter.put ("/", async (req,res)=> {
-    
+productsRouter.put ("/:pid", async (req,res) => {
+    const idProduct = req.params.pid;
+    const oldProduct = req.body; 
+    const updateProduct = await manager.updateProduct (parseInt (idProduct), oldProduct.field, oldProduct.value); 
+    res.send ({status: "Success", product: updateProduct});
 })
+
+productsRouter.delete ("/:pid", async (req,res) => {
+    const idProduct = req.params.pid; 
+    const deleteProduct = await manager.deleteProduct (parseInt(idProduct));
+    res.send ({status: "Success", product: deleteProduct});
+})
+
 
 module.exports = productsRouter;
